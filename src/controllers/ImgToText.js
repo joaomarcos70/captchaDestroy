ca2071c953bf978088918ee30c54d08f0f65a8e2
Joao Albuquerque <joaomarcos70@outlook.com.br>
2021-11-24 23:45:49 -0300

    implementando o storage do multer

--- src/controllers/ImgToText.js
+++ src/controllers/ImgToText.js
@@ -1,18 +1,27 @@
 const Tesseract = require('tesseract.js')
 
+const uploadFolder = './uploads';
+const fs = require('fs');
+/* 
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const upload = multer({ dest: 'uploads/' }); */
 
 class ImgToText {
     static async transformImgtoText(req, res) {
-
-        let img = upload.single(req.file)
-
         try {
-            Tesseract.recognize('captcha2pib.jpg', "eng", {logger:m => console.log(m)})
-            .then(result =>{
-                return res.status(200).json(result.data.text)
-            })
+
+            fs.readdir(uploadFolder, (err, files) => {
+                files.every(file => {
+                    console.log(file);
+/*                    return Tesseract.recognize(`uploads/${file}`, "eng", { logger: m => console.log(m) })
+                        .then(result => {
+                             console.log("alonicolas",result.data.text); 
+                        }) */
+                        
+                        return
+                });
+            });
+
         } catch (error) {
             console.log(error);
         }
--- src/routes/imgToText.js
+++ src/routes/imgToText.js
@@ -1,7 +1,20 @@
-const {Router} = require('express')
+const { Router } = require('express')
 const ImgToTextController = require('../controllers/ImgToText')
 const router = Router()
+const path = require('path')
 
-router.post('/img-to-txt', ImgToTextController.transformImgtoText)
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, "uploads/")
+    },
+    filename: function (req, file, callback) {
+        callback(null, file.originalname+ Date.now()+ path.extname(file.originalname))
+    }
+})
+
+const upload = multer({ storage })
+
+router.post('/img-to-txt', upload.single('file'), ImgToTextController.transformImgtoText)
 
 module.exports = router
