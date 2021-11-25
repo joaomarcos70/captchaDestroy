3c8ce2baf8e714ad0bfb0b2ab66b550ff4a1e493
João Albuquerque <joaomarcos70@outlook.com.br>
2021-11-25 14:47:08 -0300

    FINALIZANDO API DE CAPTCHA

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 captchaDestroy
+
+
+rodando api na porta 3000 
+
+rodar npm install
+
+endpoint POST /img-to-txt
+
+mandar arquivo com nome file | e carregar o arquivo normalmente como form-data
+
+retorno deverá ser um JSON
--- src/controllers/ImgToText.js
+++ src/controllers/ImgToText.js
@@ -2,24 +2,34 @@ const Tesseract = require('tesseract.js')
 
 const uploadFolder = './uploads';
 const fs = require('fs');
+var rimraf = require("rimraf");
+
+const vision = require('@google-cloud/vision')
 /* 
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' }); */
+
+const client = new vision.ImageAnnotatorClient({
+    keyFilename: './API_KEY.json'
+})
 
 class ImgToText {
     static async transformImgtoText(req, res) {
         try {
 
             fs.readdir(uploadFolder, (err, files) => {
-                files.every(file => {
-                    console.log(file);
-/*                    return Tesseract.recognize(`uploads/${file}`, "eng", { logger: m => console.log(m) })
-                        .then(result => {
-                             console.log("alonicolas",result.data.text); 
-                        }) */
-                        
-                        return
-                });
+                client
+                    .textDetection(`uploads/${files[0]}`)
+                    .then((results) => {
+                        const text = results[0].fullTextAnnotation.text
+
+                        res.status(200).json(text.replace(/(\r\n|\n|\r|\s)/gm, ""))
+                        rimraf.sync(`${uploadFolder}/${files[0]}`);
+
+                    })
+                    .catch((error) => {
+                        res.status(500).json(error)
+                    })
             });
 
         } catch (error) {
